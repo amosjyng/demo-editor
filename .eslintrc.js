cee1473853e6bd67f86cdb7dc93bdf42752827f7
Amos Ng <me@amos.ng>
2021-04-03 10:31:24 -0500

    Add linter

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "jest": true, // prevent lint errors in test
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "react/react-in-jsx-scope": "off" // React import now optional in v17
+    }
+};
