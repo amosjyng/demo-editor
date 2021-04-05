cf5a63f20772b8ac83563a729a7ad9c9e71687b3
Amos Ng <me@amos.ng>
2021-04-04 23:19:54 -0500

    Turn VariableList into a functional component

--- src/components/VariableList.js
+++ src/components/VariableList.js
@@ -3,42 +3,37 @@ import PropTypes from "prop-types";
 import Immutable from "immutable";
 import { Button, Card, Form, Col } from "react-bootstrap";
 
-class VariableList extends React.Component {
-  render() {
-    const variableListItems = [];
-    for (const variable of this.props.variables) {
-      variableListItems.push(
-        <Button variant="outline-dark" key={variable}>
-          {variable}
-        </Button>
-      );
-    }
-    return (
-      <Card body>
-        <Card.Title>Known variables</Card.Title>
-        <Form>
-          <Form.Row>
-            <Col>
-              <Form.Control
-                placeholder="Enter a new variable name"
-                onChange={this.props.updateNewVariable}
-                value={this.props.newVariable}
-              />
-            </Col>
-            <Col xs="auto">
-              <Button
-                variant="outline-primary"
-                onClick={this.props.addVariable}
-              >
-                Add variable
-              </Button>
-            </Col>
-          </Form.Row>
-        </Form>
-        <div className="variable-list">{variableListItems}</div>
-      </Card>
+export default function VariableList(props) {
+  const variableListItems = [];
+  for (const variable of props.variables) {
+    variableListItems.push(
+      <Button variant="outline-dark" key={variable}>
+        {variable}
+      </Button>
     );
   }
+  return (
+    <Card body>
+      <Card.Title>Known variables</Card.Title>
+      <Form>
+        <Form.Row>
+          <Col>
+            <Form.Control
+              placeholder="Enter a new variable name"
+              onChange={props.updateNewVariable}
+              value={props.newVariable}
+            />
+          </Col>
+          <Col xs="auto">
+            <Button variant="outline-primary" onClick={props.addVariable}>
+              Add variable
+            </Button>
+          </Col>
+        </Form.Row>
+      </Form>
+      <div className="variable-list">{variableListItems}</div>
+    </Card>
+  );
 }
 
 VariableList.propTypes = {
@@ -47,5 +42,3 @@ VariableList.propTypes = {
   updateNewVariable: PropTypes.func.isRequired,
   addVariable: PropTypes.func.isRequired,
 };
-
-export default VariableList;
